486bc60430c5db8eeb741e37151be9488ba9b5c5
Henrique Nobre <henrique.oliveira@itaiputransformadores.com.br>
2026-02-18 17:45:05 -0300

    feat(estudos): exercicios de estudos //  treinamento realizado no curso de python do theo calvo no dia 04

---
+++ dia_04/exercicio_2_15.py
@@ -0,0 +1,3 @@
+#Escreva um programa que receba uma lista de números do usuário e conte quantas vezes um número específico aparece na lista
+
+#%%
--- exercicios_medium/desafio_02.py
+++ exercicios_medium/desafio_02.py
@@ -1,10 +1,14 @@
 #Escreva um código que receba uma entrada em texto (String) e diga se a String é ou não um Palíndromo
 #%%
 #definindo variáveis
-#texto = input("Insira o texto: ")
-#
-#for i in texto:
-#    if i == texto[-i]:
-#        print ("É um palíndromo")
-#    else:
-#        print ("Não é um palíndromo")
+texto = input("Insira o texto: ")
+texto = texto.replace(" ", "").lower()
+inverso = ""
+#invertendo a palavra
+for i in range(len(texto) -1, -1, -1):
+    inverso += texto[i]
+#verificando palíndromos
+if inverso == texto:
+    print("Temos um palíndromo", texto, "=", inverso)
+else:
+    print("Essa frase", texto, "não é um palíndromo")
